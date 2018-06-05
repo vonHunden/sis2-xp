3f514f1a5d629f0a543eb8d861f3621b6d94c428
sasor <s.leamsi99@gmail.com>
2018-06-04 23:43:33 -0400

    [UPDATE index.php ver.php] archivos de las vistas

--- src/src/views/almacenes/ver.php
+++ src/src/views/almacenes/ver.php
@@ -31,12 +31,7 @@
 
           <!--BOTON PAGINA ALMACEN-->
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Almacenes</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-            </div>
+            <a class="nav-link" href="<?php echo _HOME;?>/almacenes">Almacenes</a>
           </li>
 
 
