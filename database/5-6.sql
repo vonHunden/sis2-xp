12bfb49ed3cf86c344e0df7432e8228cc87d6b3e
alexinf <alexander.unico.13@gmail.com>
2018-06-04 22:52:18 -0400

    db tarea 5- 6

--- database/5-6.sql
+++ database/5-6.sql
@@ -7,7 +7,7 @@ CREATE TABLE people
     hour VARCHAR(5) NOT NULL
 );
 
-<<<<<<< HEAD
+
 CREATE TABLE producto
 (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
@@ -23,33 +23,30 @@ CREATE TABLE categoria
 );
 
 
-INSERT INTO `categoria`(`name_categoria`) VALUES ('electronica');
+INSERT INTO `categoria`(`name_categoria`)
+ VALUES 
+ ('electronica'),
+ ('electronica'),
+ ('electronica');
+
 
 INSERT INTO `producto`(`id_categoria`, `nombre_producto`, `peso`) 
-VALUES (1,'cargador laptop','1'),
+VALUES 
+(1,'cargador laptop','1'),
 (1,'camara','1'),
 (1,'tester','0.5');
 
 
 INSERT INTO `producto`(`id_categoria`, `nombre_producto`, `peso`) 
-VALUES (2,'cable utp cat5','3'),
+VALUES 
+(2,'cable utp cat5','3'),
 (2,'switch','1.5'),
 (2,'router','1');
 
 INSERT INTO `producto`(`id_categoria`, `nombre_producto`, `peso`)
-VALUES (3,'juego de tronos','0.1'),
+VALUES 
+(3,'juego de tronos','0.1'),
 (3,'flash','0.1'),
 (3,'git','0.5');
-=======
-CREATE TABLE almacen
-(
-    idAlmacen INTEGER PRIMARY KEY AUTO_INCREMENT,
-    id_titular INTEGER,
-    nombre_almacen VARCHAR(50),
-    existencias INTEGER,
-    capacidad INTEGER
-);
-INSERT INTO `sis2-xp`.`almacen` (`idAlmacen`, `id_titular`, `nombre_almacen`, `existencias`, `capacidad`) VALUES ('1', '1', 'A12', '0', '100');
-INSERT INTO `sis2-xp`.`almacen` (`idAlmacen`, `id_titular`, `nombre_almacen`, `existencias`, `capacidad`) VALUES ('2', '1', 'B32', '40', '150');
-INSERT INTO `sis2-xp`.`almacen` (`idAlmacen`, `id_titular`, `nombre_almacen`, `existencias`, `capacidad`) VALUES ('3', '1', 'C45', '0', '100');
->>>>>>> 042a48dd28235665cdfd029229871e675153822d
+
+
